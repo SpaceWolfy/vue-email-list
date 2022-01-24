6ca1cbe8e99f054226d28075e1862dbbd771b5bd
Samuel Scozzari <s_scozzari@libero.it>
2022-01-24 16:02:17 +0100

    changed for value from 9 to 10 - exercise completed

--- vue/vue.js
+++ vue/vue.js
@@ -6,7 +6,7 @@ new Vue({
   },
   methods: {
     newMail: function () {
-      for (i = 0; i < 9; i++) {
+      for (i = 0; i < 10; i++) {
         axios
           .get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then((result) => {
